551ecbeec0199a0c788fd45a1d6fb96b852a820c
Tristan Blasin <trblasin@gmail.com>
2023-01-02 23:18:27 +0400

    update on patern/package.json

--- src/index.jsx
+++ src/index.jsx
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './styles/index.css';
 import RoutesPath from './RoutesPath';
 
-// Création du système de routage entre les différentes pages du site web :
+// Création du lien d'accès à RoutesPath qui gère le routage de nos pages web :
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   
      <React.StrictMode>
-      <RoutesPath/>
+      <RoutesPath />
      </React.StrictMode>
     
 );
